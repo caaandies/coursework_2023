1137bd1a15c7b5f28d30ac0feff43f2a59879354
caaandies <AndreasyanAndrey@mail.ru>
2023-05-27 03:03:58 +0300

    massive code rework

--- functions.py
+++ functions.py
@@ -1,10 +1,8 @@
 import xarray as xr
-import requests
 from urllib.request import urlopen
 from lxml import etree
 import os
 import wget
-import cdsapi
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,14 +10,8 @@ import cartopy.crs as ccrs
 from scipy.sparse.linalg import lsmr
 from matplotlib.animation import FuncAnimation
 import datetime
-from dateutil.relativedelta import relativedelta
+from tempfile import NamedTemporaryFile
 
-
-
-file_dict = {
-             "daily mslp": "daily_mslp.nc",
-             "monthly mslp": "monthly_mslp.nc"
-             }
 
 def get_corr(vector1, vector2):
     return np.corrcoef(vector1, vector2)[0][1]
@@ -38,118 +30,125 @@ def update_data():
                 ]
     for index in range(len(urls)):
         file_names = etree.parse(urlopen(urls[index]), etree.HTMLParser()).xpath('//a[contains(@href, "mslp")]/@href')
-        
         dataset = None
-
         for file in sorted(file_names):
             f = wget.download(f"{urls[index]}{file}")
-            
             if dataset is None:
                 dataset = xr.open_dataset(f)
             else:
                 dataset = xr.concat([dataset, xr.open_dataset(f)], dim = 'time')
-
             os.remove(f)
-
         dataset.to_netcdf(ds_names[index])
 
 
-def get_current_state(date, data):
-    strdate = date.strftime("%Y-%m-%d")
-    if data == "monthly mslp":
-        strdate = date.strftime("%Y-%m")
+class Forecast():
+    def __init__(self, data, date, duration, init_state):
+        self.data = data # daily_mslp.nc or monthly_mslp.nc
+        self.date = date # datetime.date
+        self.duration = duration # int > 0
+        self.init_state = init_state # netcdf file or None
     
-    return xr.open_dataset(file_dict[data]).sel(time=strdate)
+    def get_current_state(self):
+        if self.init_state is not None:
+            with NamedTemporaryFile() as tmp:
+                tmp.write(self.init_state.get_value())
+                return xr.open_dataset(tmp.name)
+        if self.data == "daily_mslp.nc":
+            strdate = self.date.strftime("%Y-%m-%d")
+        elif self.data == "monthly_mslp.nc":
+            strdate = self.date.strftime("%Y-%m")
+        return xr.open_dataset(self.data).sel(time=strdate)
+
+
+    def natural_analogue(self):
+        six_months_ago = self.date - pd.DateOffset(months=6)
+        historical_data = xr.open_dataset(self.data).sel(time=slice(None, six_months_ago))
+        historical_mslp = historical_data['mslp'].values
+        original_shape = historical_mslp.shape[1]
+        historical_mslp = historical_mslp.reshape(historical_mslp.shape[0], -1)
+
+        current_mslp = self.get_current_state()['mslp'].values.flatten()
+
+        max_corr = -2
+        analog_time = 0;
+        for time in range(historical_mslp.shape[0]):
+            corr = get_corr(current_mslp, historical_mslp[time])
+            if corr > max_corr:
+                max_corr = corr
+                analog_time = time
+        analogue_time = pd.to_datetime(str(historical_data['time'].values[analog_time]))
+        if self.data == "daily_mslp.nc":
+            analogue_time = analogue_time.strftime('%Y-%m-%d')
+        elif self.data == "monthly_mslp.nc":
+            analogue_time = analogue_time.strftime('%Y-%m')
+        return (
+                current_mslp.reshape((original_shape, -1)),
+                historical_mslp[analog_time + 1: analog_time + self.duration + 1].reshape((self.duration, original_shape, -1)),
+                historical_mslp[analog_time].reshape((original_shape, -1)),
+                get_rmse(current_mslp, historical_mslp[analog_time]),
+                max_corr,
+                analogue_time
+                )
+
+
+
+    def calculated_analogue(self):
+        six_months_ago = self.date - pd.DateOffset(months=6)
+        historical_data = xr.open_dataset(self.data).sel(time=slice(None, six_months_ago))
+        historical_mslp = historical_data['mslp'].values
+        original_shape = historical_mslp.shape[1]
+        historical_mslp = historical_mslp.reshape(historical_mslp.shape[0], -1).T
+
+        current_mslp = self.get_current_state()['mslp'].values.flatten()
+
+        coeffs = lsmr(historical_mslp, current_mslp)[0]
+        analogue = historical_mslp @ coeffs
+        return (
+                current_mslp.reshape((original_shape, -1)),
+                np.array([(historical_mslp[:, i:] @ coeffs[:-i]).reshape((original_shape, -1)) for i in range(1, self.duration + 1)]),
+                analogue.reshape((original_shape, -1)),
+                get_rmse(current_mslp, analogue),
+                get_corr(current_mslp, analogue)
+                )
+
+
+
+    def make_picture(self, mslp, title=""):
+        fig = plt.figure(figsize=(10, 5))
+        ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
+        ax.coastlines()
+        ax.set_title(title)
+        image = ax.imshow(mslp,
+                          extent=[0, 360, -90, 90],
+                          transform=ccrs.PlateCarree())
+        fig.colorbar(image, ax=ax, orientation='vertical')
+        return fig
     
+    def make_animation(self, mslp):
+        fig = plt.figure(figsize=(10, 5))
+        ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
+        ax.coastlines()
 
-def natural_analogue(date, data, time_units):
-    six_months_ago = date - pd.DateOffset(months=6)
-    historical_data = xr.open_dataset(file_dict[data]).sel(time=slice(None, six_months_ago))
-    historical_mslp = historical_data['mslp'].values
-    original_shape = historical_mslp.shape[1]
-    historical_mslp = historical_mslp.reshape(historical_mslp.shape[0], -1)
-
-    current_mslp = get_current_state(date, data)['mslp'].values.flatten()
-
-    max_corr = -2
-    analog_time = 0;
-    rmse = 0;
-    for time in range(historical_mslp.shape[0]):
-        corr = get_corr(current_mslp, historical_mslp[time])
-        if corr > max_corr:
-            max_corr = corr
-            analog_time = time
-    str_time = str(historical_data['time'].values[analog_time])
-    rmse = get_rmse(current_mslp, historical_mslp[analog_time])
-    return (
-            current_mslp.reshape((original_shape, -1)),
-            historical_mslp[analog_time + 1: analog_time + time_units + 1].reshape((time_units, original_shape, -1)),
-            historical_mslp[analog_time].reshape((original_shape, -1)),
-            rmse,
-            corr,
-            str_time
-            )
-
-
-def calculated_analogue(date, data, time_units):
-    six_months_ago = date - pd.DateOffset(months=6)
-    historical_data = xr.open_dataset(file_dict[data]).sel(time=slice(None, six_months_ago))
-    historical_mslp = historical_data['mslp'].values
-    original_shape = historical_mslp.shape[1]
-    historical_mslp = historical_mslp.reshape(historical_mslp.shape[0], -1).T
-
-    current_mslp = get_current_state(date, data)['mslp'].values.flatten()
-
-    coeffs = lsmr(historical_mslp, current_mslp)[0]
-    analogue = historical_mslp @ coeffs
-    rmse = get_rmse(current_mslp, analogue)
-    corr = get_corr(current_mslp, analogue)
-    return (
-            current_mslp.reshape((original_shape, -1)),
-            np.array([(historical_mslp[:, i:] @ coeffs[:-i]).reshape((original_shape, -1)) for i in range(1, time_units + 1)]),
-            (historical_mslp @ coeffs).reshape((original_shape, -1)),
-            rmse,
-            corr
-            )     
-
-
-
-def make_picture(data, title=""):
-    fig = plt.figure(figsize=(10, 5))
-    ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
-    ax.coastlines()
-    ax.set_title(title)
-    image = ax.imshow(data, origin='upper',
-                  extent=[0, 360, -90, 90],
-                  transform=ccrs.PlateCarree(),
-                  cmap='viridis')
-    fig.colorbar(image, ax=ax, orientation='vertical')
-    return fig
-
-def make_animation(mslp, date, data):
-    fig = plt.figure(figsize=(10, 5))
-    ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
-    ax.coastlines()
-
-    image = ax.imshow(mslp[0, :, :],
-                      extent=[0, 360, -90, 90],
-                      transform=ccrs.PlateCarree())
-    
-    step = datetime.timedelta(days=1)
-    format = '%Y-%m-%d'
-    
-    if (data == 'monthly mslp'):
-        step = relativedelta(months=1)
-        format = '%Y-%m'
+        image = ax.imshow(mslp[0, :, :],
+                          extent=[0, 360, -90, 90],
+                          transform=ccrs.PlateCarree())
         
+        if self.data == "daily_mslp.nc":
+            step = datetime.timedelta(days=1)
+            format = '%Y-%m-%d'
+        elif self.data == "monthly_mslp.nc":
+            # step = relativedelta(months=1)
+            step = pd.DateOffset(months=1)
+            format = '%Y-%m' 
 
-    text = ax.text(0.5, 1.01, date.strftime(format),
-                   transform=ax.transAxes, ha='center')
+        text = ax.text(0.5, 1.01, self.date.strftime(format),
+                       transform=ax.transAxes, ha='center')
+        fig.colorbar(image, ax=ax, orientation='vertical')
     
-    def update(index):
-        image.set_array(mslp[index, :, :])
-        text.set_text((date + (index + 1) * step).strftime(format))
-        return image, text
+        def update(index):
+            image.set_array(mslp[index, :, :])
+            text.set_text((self.date + (index + 1) * step).strftime(format))
+            return image, text
     
-    anim = FuncAnimation(fig, update, frames=range(mslp.shape[0]), interval=500, blit=True)
-    anim.save('forecast.gif')
+        anim = FuncAnimation(fig, update, frames=range(mslp.shape[0]), interval=500, blit=True)
+        anim.save('forecast.gif')
--- main.py
+++ main.py
@@ -1,40 +1,32 @@
 import datetime
 from functions import *
+import numpy as np
 import streamlit as st
 from streamlit_option_menu import option_menu
-from io import StringIO
-
-time_units_dict = {
-              "daily mslp": "days",
-              "monthly mslp": "months"
-              }
 
 with st.sidebar:
-    selected = option_menu("Main Menu", ["Home", "Forecasts", "Data"], 
+    selected_option = option_menu("Main Menu", ["Home", "Forecasts", "Data"], 
         icons=["house", "cloud-drizzle", "clipboard-data"], menu_icon="cast")
 
-if selected == "Home":
+if selected_option == "Home":
     st.title("Hello world!")
     st.markdown("This is a local web application that will allow you to use the program using a graphical interface")
 
-elif selected == "Forecasts":
+elif selected_option == "Forecasts":
     st.subheader("Choose parameters")
     with st.container():
         col1, col2 = st.columns(2)
 
-        method = col1.radio(
+        selected_method = col1.radio(
             'Which method do you want to use?',
             ['Natural analogue', 'Calculated analogue']
         )
 
-        data = col2.radio(
+        selected_data = col2.radio(
             'On what data do you want to use the method?',
             ['daily mslp', 'monthly mslp']
         )
     st.subheader("Set the initial state")
-
-    
-
     
     today = datetime.date.today()
     start_date = datetime.date(2000, 1, 1)
@@ -42,25 +34,38 @@ elif selected == "Forecasts":
 
     selected_date = st.date_input("Enter the date", start_date, min_value=start_date, max_value=end_date)
     st.write("Your date:", selected_date)
+    if selected_data == "daily mslp":
+        units = "days"
+        file = "daily_mslp.nc"
+    elif selected_data == "monthly mslp":
+        units = "months"
+        file = "monthly_mslp.nc"
+    duration = st.number_input(f'Enter the forecast duration in {units}', step=1, min_value=1)
 
-    duration = st.number_input(f'enter the forecast duration in {time_units_dict[data]}', step=1, min_value=1)
+    st.markdown("You can upload a file with the initial state, otherwise the initial state will be considered the state at the time of the entered date")
+    uploaded_file = st.file_uploader("Choose a file", type=['nc'])
 
     if st.button("Make forecast"):
         with st.spinner('Wait for it...'):
-            if method == "Natural analogue":
-                init, forecast, analog, rmse, corr, analog_time = natural_analogue(selected_date, data, duration)
-                st.pyplot(make_picture(init), "initial state")
-                st.pyplot(make_picture(analog), f"natural analog from {analog_time} with rmse={rmse} correlation={corr}")
-                make_animation(forecast, selected_date, data)
+            forecast = Forecast(file, selected_date, duration, uploaded_file)
+            if selected_method == "Natural analogue":
+                init, prediction, analogue, rmse, corr, analogue_time = forecast.natural_analogue()
+                rmse = np.round(rmse, decimals=2)
+                corr = np.round(corr, decimals=4)
+                st.pyplot(forecast.make_picture(init, "initial state"))
+                st.pyplot(forecast.make_picture(analogue, f"natural analogue ({analogue_time}, rmse={str(rmse)}, corr={str(corr)})"))
+                forecast.make_animation(prediction)
                 st.image('forecast.gif')
-            else:
-                init, forecast, analog, rmse, corr = calculated_analogue(selected_date, data, duration)
-                st.pyplot(make_picture(init), "initial state")
-                st.pyplot(make_picture(analog), "calculated analogue with rmse={rmse} correlation={corr}")
-                make_animation(forecast, selected_date, data)
+            elif selected_method == "Calculated analogue":
+                init, prediction, analog, rmse, corr = forecast.calculated_analogue()
+                rmse = np.round(rmse, decimals=2)
+                corr = np.round(corr, decimals=2)
+                st.pyplot(forecast.make_picture(init, "initial state"))
+                st.pyplot(forecast.make_picture(analog, f"calculated analogue (rmse={str(rmse)}, corr={str(corr)})"))
+                forecast.make_animation(prediction)
                 st.image('forecast.gif')
         
-elif selected == "Data":
+elif selected_option == "Data":
     st.subheader('Do you want to upload/update data?')
     if st.button('Upload/Update'):
         with st.spinner('Wait for it...'):
